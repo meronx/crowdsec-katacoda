5f4ba9cc7880697fa9ea615a57ad0c1d979179c5
Meron <45291090+meronx@users.noreply.github.com>
2022-02-08 23:14:51 +0100

    Update env-init.sh

--- crowdsec-setup2/env-init.sh
+++ crowdsec-setup2/env-init.sh
@@ -1,2 +1,2 @@
 # Install apache2 service for CrowdSec auto detection
-ssh root@host01 "apt install apache2 -y; service apache2 restart"
+ssh root@host01 "apt install apache2 -y && systemctl restart apache2"
